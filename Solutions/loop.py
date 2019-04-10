8783a78666eb41451c485bee3489fe77d501f670
Mehmet Berk Cetin <43109419+CetinMehmet@users.noreply.github.com>
2019-04-10 14:25:20 +0200

    Delete loop.py

--- Solutions/loop.py
+++
@@ -1,7 +0,0 @@
-"""
-  Task 
-  Read an integer . For all non-negative integers , print . See the sample for details.
-"""
-if __name__ == '__main__':
-    [print(i**2) for i in range(int(input()))]
-
